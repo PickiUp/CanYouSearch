fb288a7a838c092af0ee8c071034d1028faf7567
PickiUp <lzy2001@proton.me>
2024-02-10 11:28:30 +0800

    feat: 基础站点引导

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import Joyride from 'react-joyride';
 import SearchInput from './components/SearchInput';
 import SearchSettings from './components/SearchSettings';
 import SearchTabs from './components/SearchTabs';
@@ -16,6 +17,25 @@ function App() {
     const [settingConfig, setSettingConfig] = useState(settingsConfig);
 
     const [hasReload, setHasReload] = useState(true);
+
+    const steps = [
+        {
+            target: '.searchInput  .ant-input-affix-wrapper',
+            content: '输入关键字',
+        },
+        {
+            target: '.app header .searchSettings .ant-collapse .ant-collapse-expand-icon',
+            content: '点击按钮打开设置',
+        },
+        {
+            target: '.ant-tabs #rc-tabs-1-panel-0 .ant-list-items .list-item .pic',
+            content: '鼠标移入查看网址项名称',
+        },
+        {
+            target: '.ant-tabs #rc-tabs-1-panel-0 .ant-list-items .ant-input-group-addon',
+            content: '点击查询按钮进行搜索',
+        },
+    ];
 
     return (
         <div className="app">
@@ -39,6 +59,12 @@ function App() {
                 />
             </main>
             {/* <footer>footer</footer> */}
+            <Joyride
+                steps={steps}
+                continuous={true}
+                showProgress={true}
+                showSkipButton={true}
+            />
         </div>
     );
 }
